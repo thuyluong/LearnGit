7811bb06dc2a84d445810adfcccfb640c85372c4
TaiVuong <vhuutai@gmail.com>
2014-12-13 17:52:29 +0700

    Finish register account

---
+++ LearnGit/LearnGit/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  LearnGit
+//
+//  Created by TaiVuong on 12/13/14.
+//  Copyright (c) 2014 TaiVuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
